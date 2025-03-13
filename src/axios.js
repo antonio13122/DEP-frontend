3425534cdd4843a6097c1f7f2da3360f385a86b0
Antonio <tonisajbic501@gmail.com>
2025-03-13 18:05:38 +0100

    deploy prep

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://desolate-caverns-71958-8003a607a2e2.herokuapp.com/",
   headers: {
     "Content-Type": "application/json",
   },
